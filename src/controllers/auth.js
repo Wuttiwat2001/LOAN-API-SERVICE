e78cd8b964e7db531dbf4618a4287e2aeaf95841
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-18 19:54:27 +0700

    login system with jwt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,8 @@
 const prisma = require("../prisma/prisma");
 const hashSync = require("bcrypt").hashSync;
+const compareSync = require("bcrypt").compareSync;
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets");
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -24,10 +27,32 @@ const signup = async (req, res) => {
   res.status(201).json(user);
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
+  const user = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: email }, { username: email }],
+    },
+  });
 
+  if (!user) {
+    return res.status(400).json({ error: "Invalid email or username" });
+  }
 
+  const isPasswordValid = compareSync(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ error: "Invalid password" });
+  }
+
+  const token = jwt.sign({ id: user.id }, JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.status(200).json({ user, token });
+};
 
 module.exports = {
   signup,
+  login,
 };
