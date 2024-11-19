ddc8fd2739310c566d355a07d22391a4d18ca7d1
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-19 15:38:46 +0700

    route requestBorrow define

--- src/routes/request.routes.js
+++ src/routes/request.routes.js
@@ -3,6 +3,6 @@ const requestRoutes = express.Router();
 const authMiddleware = require("../middlewares/auth");
 const { requestBorrow } = require("../controllers/request.controller");
 
-requestRoutes.post("/req", [authMiddleware], requestBorrow);
+requestRoutes.post("/requestBorrow", [authMiddleware], requestBorrow);
 
 module.exports = requestRoutes;
