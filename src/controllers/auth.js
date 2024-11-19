db0c74e3fb505ff36fb56c60cc622a187ef6aa27
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-19 10:18:11 +0700

    add try catch

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,51 +5,61 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../secrets");
 
 const signup = async (req, res) => {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  const existingUser = await prisma.user.findFirst({
-    where: {
-      OR: [{ username: username }, { email: email }],
-    },
-  });
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ username: username }, { email: email }],
+      },
+    });
 
-  if (existingUser) {
-    return res.status(400).json({ error: "Username or email already exists" });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ error: "Username or email already exists" });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashSync(password, 10),
+      },
+    });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashSync(password, 10),
-    },
-  });
-  res.status(201).json(user);
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await prisma.user.findFirst({
-    where: {
-      OR: [{ email: email }, { username: email }],
-    },
-  });
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: email }, { username: email }],
+      },
+    });
 
-  if (!user) {
-    return res.status(400).json({ error: "Invalid email or username" });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid email or username" });
+    }
+
+    const isPasswordValid = compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  const isPasswordValid = compareSync(password, user.password);
-  if (!isPasswordValid) {
-    return res.status(400).json({ error: "Invalid password" });
-  }
-
-  const token = jwt.sign({ id: user.id }, JWT_SECRET, {
-    expiresIn: "1d",
-  });
-
-  res.status(200).json({ user, token });
 };
 
 module.exports = {
