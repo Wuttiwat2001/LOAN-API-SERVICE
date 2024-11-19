9b47d38b969db6415e8ea0c41755fc14a80f34f3
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-19 13:32:48 +0700

    transaction route define

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const rootRouter = express.Router();
 const authRoutes = require("./auth");
+const transactionRoutes = require("./transaction");
 
 rootRouter.use("/auth", authRoutes);
+rootRouter.use("/transaction", transactionRoutes);
 
 module.exports = rootRouter;
