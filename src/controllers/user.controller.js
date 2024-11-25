432d583040945c4d6a67b1efff48d812edc8e89d
KhunCaptain <captenlnw_za@hotmail.com>
2024-11-26 02:42:56 +0700

    find user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,6 +15,31 @@ const allUsers = async (req, res) => {
   }
 };
 
+const findUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json({
+      data: {
+        message: "SUCCESS",
+        user: user,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 module.exports = {
   allUsers,
+  findUserById,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userRoutes = express.Router();
 const authMiddleware = require("../middlewares/auth");
-const { allUsers } = require("../controllers/user.controller");
+const { allUsers, findUserById } = require("../controllers/user.controller");
 
 userRoutes.get("/", [authMiddleware], allUsers);
+userRoutes.get("/users/:id", [authMiddleware], findUserById);
 
 module.exports = userRoutes;
