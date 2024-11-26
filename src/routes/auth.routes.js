788c22f1b784ec9d196545df58ad7c0ecd4d376a
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-26 11:42:50 +0700

    validator signup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const authRoutes = express.Router();
 const { signup, login } = require("../controllers/auth.controller");
-const { validateLogin } = require("../validators/authValidator");
+const {
+  validateLogin,
+  signupValidationRules,
+} = require("../validators/authValidator");
 const validationMiddleware = require("../middlewares/validation");
 
-authRoutes.post("/signup", signup);
+authRoutes.post("/signup", validateLogin, signupValidationRules, signup);
 authRoutes.post("/login", validateLogin, validationMiddleware, login);
 
 module.exports = authRoutes;
--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -1,8 +1,19 @@
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
 const validateLogin = [
-  body('username').isString().notEmpty().withMessage('Username is required'),
-  body('password').isString().notEmpty().withMessage('Password is required'),
+  body("username").isString().notEmpty().withMessage("Username is required"),
+  body("password").isString().notEmpty().withMessage("Password is required"),
 ];
 
-module.exports = { validateLogin };
+const signupValidationRules = () => {
+  return [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().withMessage("Email is required"),
+    body("password").notEmpty().withMessage("Password is required"),
+    body("firstName").notEmpty().withMessage("First name is required"),
+    body("lastName").notEmpty().withMessage("Last name is required"),
+    body("phone").notEmpty().withMessage("Phone number is required"),
+  ];
+};
+
+module.exports = { validateLogin, signupValidationRules };
