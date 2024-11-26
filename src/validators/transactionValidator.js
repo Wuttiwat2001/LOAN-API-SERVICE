c565c9c68678ed4ab841e97054af6f04a661210a
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-26 15:53:56 +0700

    transaction validator

---
+++ src/validators/transactionValidator.js
@@ -0,0 +1,21 @@
+const { body, param, query } = require("express-validator");
+
+const validateGetTransactionsByUserId = [
+  body("userId")
+    .isInt()
+    .withMessage("User ID ต้องเป็นตัวเลข")
+    .notEmpty()
+    .withMessage("กรุณาระบุ User ID"),
+  query("page")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage("Page ต้องเป็นตัวเลขที่มากกว่าหรือเท่ากับ 1"),
+  query("pageSize")
+    .optional()
+    .isInt({ min: 1 })
+    .withMessage("Page Size ต้องเป็นตัวเลขที่มากกว่าหรือเท่ากับ 1"),
+];
+
+module.exports = {
+  validateGetTransactionsByUserId,
+};
