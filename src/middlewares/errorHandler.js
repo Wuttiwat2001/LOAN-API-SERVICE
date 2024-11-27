cbde4658b31938cbbfe7912ac319dc42b6e59242
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-11-27 09:38:45 +0700

    fix text error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    res.status(500).json({ error: "เซิร์ฟเวอร์เกิดข้อผิดพลาด" });
+    res.status(500).json({ error: "เซิร์ฟเวอร์เกิดข้อผิดพลาดโปรดลองใหม่อีกครั้งภายหลัง" });
   };
   
   module.exports = errorHandler;
